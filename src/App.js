55d421e81aabc0984d71e555a522d49ae51effda
SofiaPinilla <contact@sofiapinilla.com>
2022-12-12 10:14:24 +0100

    actualizacion login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,14 +1,24 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import authService from "./authService";
 
+
+const user = JSON.parse (localStorage.getItem("user"))
 const initialState = {
-  user: null,
+  user: user?user:null,
 };
+
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
+  extraReducers:(builder) => {
+    builder
+    .addCase (login.fulfilled,(state,action) =>  {
+
+        state.user = action.payload
+    })
+  }
 });
 
 export const register = createAsyncThunk("auth/register", async (user) => {
@@ -19,5 +29,14 @@ export const register = createAsyncThunk("auth/register", async (user) => {
 
   }
 });
+
+export const login = createAsyncThunk ("auth/login", async (user) => {
+
+    try {
+        return await authService.login (user)
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 export default authSlice.reducer;
